50773d63c5418193130ef249813ca293a9ae7b56
revaria <tanchunliang2003@gmail.com>
2026-03-20 03:36:06 +0800

    Add logging and assertion in Parser

--- src/main/java/fitlogger/parser/Parser.java
+++ src/main/java/fitlogger/parser/Parser.java
@@ -7,15 +7,21 @@ import fitlogger.exception.FitLoggerException;
 import fitlogger.workoutlist.WorkoutList;
 import fitlogger.storage.Storage;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Parser {
     // temporary for now, change later when we know how much description there is
     private static final int MAX_LIFT_INFO = 100;
     private static final int MAX_RUN_INFO = 50;
+    private static Logger logger = Logger.getLogger("Foo");
 
     public static Command parse(String fullCommand,
             WorkoutList workouts,
             Storage storage)
             throws FitLoggerException {
+        logger.log(Level.INFO, "going to start parsing");
+        assert fullCommand != null : "Parser.parse was called with a null string!";
         String[] parts = fullCommand.trim().split(" ", 2);
         String commandWord = parts[0].toLowerCase();
         String arguments = (parts.length > 1) ? parts[1].trim() : "";
