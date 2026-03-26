80b3acfa3103271af55a4392410085a94082ff4f
Javierseow <seowjackiejavier@gmail.com>
2026-03-26 21:07:41 +0800

    Fix import statements in parser

--- src/main/java/fitlogger/parser/Parser.java
+++ src/main/java/fitlogger/parser/Parser.java
@@ -1,6 +1,17 @@
 package fitlogger.parser;
 
-import fitlogger.command.*;
+
+import fitlogger.command.AddWorkoutCommand;
+import fitlogger.command.Command;
+import fitlogger.command.DeleteCommand;
+import fitlogger.command.EditCommand;
+import fitlogger.command.ExitCommand;
+import fitlogger.command.HelpCommand;
+import fitlogger.command.UpdateProfileCommand;
+import fitlogger.command.ViewDatabaseCommand;
+import fitlogger.command.ViewHistoryCommand;
+import fitlogger.command.ViewProfileCommand;
+import fitlogger.command.ViewShoeMileageCommand;
 import fitlogger.exception.FitLoggerException;
 import fitlogger.workout.RunWorkout;
 import fitlogger.workout.StrengthWorkout;
